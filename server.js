c73a08e4232ebcef3268b9bb7ed9096c02af36bf
mr-selviraja <i.am.selviraja.p@gmail.com>
2023-04-28 15:06:38 +0530

    Add errorHandler middleware for handling Errors

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 
 // local modules and packages
 const contactRoutes = require("./routes/contactRoutes.js");
+const errorHandler = require("./middleware/errorHandler.js");
 
 // initialize express into app
 const app = express();
@@ -12,10 +13,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // MIDDLEWARES
-// middlewares for routing
+// for parsing data received from the client
+app.use(express.json());
+// for routing
 app.use("/api/contacts", contactRoutes);
+// for errorHandling
+app.use(errorHandler);
 
-// app starting
+// run app on the defined port
 app.listen(port, () => {
   console.log(`Server is running on PORT: ${port}`);
 });
