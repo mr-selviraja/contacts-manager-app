79c62e74d195d8e3b65b5d37b909399d4e23e222
mr-selviraja <i.am.selviraja.p@gmail.com>
2023-04-30 15:23:47 +0530

    Verifying user with the JWT token

--- controllers/userController.js
+++ controllers/userController.js
@@ -78,7 +78,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/current
 // @access PRIVATE
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information!" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
